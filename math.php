8bd5fea1d7bb3561d9d1fa8ae6a0c3c5902de964
wright0776 <matwright2010@gmail.com>
2018-07-15 15:29:02 -0600

    initial commit

---
+++ math.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "addition:";
+    echo "<br>";
+    echo 56 + 45;
+    echo "<br>";
+    echo "subtraction:";
+    echo "<br>";
+    echo 34 - 12;
+    echo "<br>";
+    echo 50 / 5;
+    echo "<br>";
+    echo 45 + 34 * 45 / 12 - 45;
+    echo "<br>";
+    echo 5 + 5 * 10;
+    echo "<br>";
+    $number1 = 12;
+    $number2 = 24;
+
+    echo "adding vars:";
+    echo "<br>";
+    echo $number1 + $number2;
+    ?>
+</body>
+</html>
